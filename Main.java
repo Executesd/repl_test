47d36611c76e0c9595e3eaf242836c38f7c3a1b1
Mohammed Hassan <weekmo@aol.com>
2021-02-17 18:12:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    for(int i=1;i<11;i++){
+      System.out.println("Hello world!"+i);
+    }
+  }
+}
