cf9c00f3e994eaf7282b3beda78f20cd4df91dc2
Mohammed Hassan <weekmo@aol.com>
2021-02-17 18:34:47 +0000

    add 1

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     for(int i=1;i<11;i++){
-      System.out.println("Hello world!"+i);
+      System.out.println("Hello world!"+i+1);
     }
   }
 }
